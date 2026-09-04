dd400241a6f0640aabf3f53463a90a492e80c3a5
sujith-23 <chavasaisujith06@gmail.com>
2026-09-04 14:32:34 +0530

    Fix production CORS

--- app/main.py
+++ app/main.py
@@ -34,7 +34,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://127.0.0.1:5500",
         "http://localhost:5500",
-        "https://sujith-23.github.io",],  # tighten to your GitHub Pages origin before going live
+        "https://sujith-23.github.io",],
+          allow_credentials=True,  # tighten to your GitHub Pages origin before going live
     allow_methods=["*"],
     allow_headers=["*"],
 )
